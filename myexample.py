79f28068d629a06b13196167de14d1e52e93686d
rbtprograms <rbt61690@gmail.com>
2018-10-21 18:45:53 -0700

    lets do it

--- myexample.py
+++ myexample.py
@@ -1,1 +1,4 @@
 print('hello world its bobby')
+7%4
+5+2
+13-8
---
+++ notes.py
@@ -0,0 +1,23 @@
+#data types
+
+#int - numbers
+#float - numbers with decimal points
+#strings - ordered sequence of chatacters
+#list - equivilant of JS arrays
+#dict - Key value pairs, like objects. First look like JSON.
+#tup (tuples) - ordered, immutable sequence of objects: (10, 'hello', 200.3). Can't be changed;
+#set (sets) - unordered collection of items, no key value pairs but they are unique.
+#bool - booleans. True and False need to be capitalized.
+
+#Numbers - int and float
+#basically the same thing
+#modulo also in python
+# power is denoted with **
+#order of ops works as well
+
+#variables
+#variable names cant start with number, cant have spaces, cant use weird symbols
+#names should be lowercase and dont use words that have a special meaning in python
+#python uses dynamic typing
+#can reassign variables to different data types, this works in javascript too
+
