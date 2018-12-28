931567039a03b6c0e7e06f2e565cd3ff6a3537f9
rbtprograms <rbt61690@gmail.com>
2018-12-28 07:03:19 -0800

    all done with strings!

--- strings.py
+++ strings.py
@@ -22,3 +22,42 @@ print(str[::3])
 print(str[::-1])
 #this will reverse your string, start at beginning, go to end, each loop should go backwards
 # and python has negative indeces which is nice
+
+#in python strings are immutable
+#to make new strings out of old strings you must concatenate
+
+x = 'Hello World '
+print(x + 'it is beautiful')
+x = x + ' ADDED '
+print(x)
+#you can multiply strings
+print(x * 3)
+
+testStr = 'Hello World'
+#how to upper case
+print(testStr.upper())
+#how to lower
+print(testStr.lower())
+#how to split into list
+#defaults to split on whitespace
+#can pass an arguement of something to split on
+print(testStr.split())
+
+#formatting strings to inject values
+print('This is a {}. And this is {} string.'.format('string', 'another'))
+##you can put index positions in the curlys and it grab the word at that index in the format call
+print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
+#you can also declare and use variables
+print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+
+#float formats
+result = 100/77
+              #first number is width, adds space in front of number
+              #second number is how many decimals you want to go to
+print('*RESULTS: {r:1.5f}**'.format(r=result))
+
+#formatting with string literals
+name = 'Bobby'
+age = 28
+#putting f infront of string lets you add variables
+print(f'Hello {name} you are {age} years old.')
